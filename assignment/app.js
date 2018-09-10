c3eac7aab1de79811a39d2c5f9be66427030b971
Sonali02 <33029448+Sonali02@users.noreply.github.com>
2018-09-10 10:13:59 +0530

    Update app.js

--- assignment/app.js
+++ assignment/app.js
@@ -1,1 +1,18 @@
-
+(function () {
+'use strict';
+ angular.module('myctrl',[])
+ .controller('myctrl',function($scope){
+  $scope.name="";
+  $scope.totalValue=0;
+  $scope.displayNUmeric=function(){
+  var totalNameValue=calculateNumericForString($scope.name);
+  $scope.totalvalue=totalNameValue;
+  };
+  function calculateNumericForString(string){
+  var totalStringValue=0;
+  for(var i=0;i<string.length;i++){
+  totalStringValue+=string.charCodeAt(i);
+  }
+  return totalStringValue;
+  });
+ })();
